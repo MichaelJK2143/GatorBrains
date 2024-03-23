0566848063f5932c289ed3842650006418145af0
MichaelJK2143 <michaeljohnknauf@gmail.com>
2024-03-23 15:48:36 -0400

    revised model

--- Backend/models.py
+++ Backend/models.py
@@ -1,30 +1,35 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ARRAY
 from datetime import datetime, timedelta
+from sqlalchemy import ARRAY
 
-db = SQLAlchemy()
+from app import db
 
-class Course():
-     def __init__(self, course_name: str, course_code: str, professor: str):
-          self.course_name = course_name
-          self.course_code = course_code[:8] # shouldn't be more than 8, right?
-          self.professor = professor
+class course():
+     name = db.Column(db.String(80))
+     course_code = db.Column(db.String(8))
+     professor = db.Column(db.String(40))
      
+     def __repr___(self):
+          return '<course %r>' % self.name
+
      
 class StudySesh():
+     id = db.Column(db.Integer, primary_key=True)
      start_time = db.Column(db.DateTime, default=datetime.now()) # time it was created
      departure_time = db.Column(db.DateTime, default=lambda: datetime.now() + timedelta(hours=1)) # default to hour later from created
-     study_subject = db.Column(db.Course)
+     course = db.Column(db.course)
 
-
+     def __repr___(self):
+          return '<id %r>' % self.course_name
+     
 class User(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(80))
      username = db.Column(db.String(80))
      password = db.Column(db.String(80))
      email = db.Column(db.String(80)) # try to figure this out eventually
-     schedule = db.Column(ARRAY(Course))
+     schedule = db.Column(ARRAY(db.Course))
      
 
      def __repr__(self):
           return '<User %r>' % self.id
+     
