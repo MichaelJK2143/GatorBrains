38b33fe1a01bbf1c8df0bef7845e5b75bc1d36c0
MichaelJK2143 <michaeljohnknauf@gmail.com>
2024-03-23 19:48:36 -0400

    add coordinates to model

--- Backend/models.py
+++ Backend/models.py
@@ -12,6 +12,7 @@ class Course(db.Model):
      course_code = db.Column(db.String(8))
      # omg, this could also work if somebody says like MCAT or smthg
      professor = db.Column(db.String(40))
+
      
      def __repr___(self):
           return '<course %r>' % self.course_code
@@ -22,10 +23,14 @@ class StudySesh(db.Model):
      start_time = db.Column(db.DateTime, default=datetime.now()) # time it was created
      departure_time = db.Column(db.DateTime, default=lambda: datetime.now() + timedelta(hours=1)) # default to hour later from created
      course = db.Column(db.String(8))
+     x = db.Column(db.Float)
+     y = db.Column(db.Float)
 
-     def __init__(self, duration, course):
+     def __init__(self, duration, course, x, y):
           self.departure_time = self.start_time + datetime.timedelta(hours=duration)
           self.course = course
+          self.x = x
+          self.y = y
 
      def __repr___(self):
           return '<id %r>' % self.course_name
