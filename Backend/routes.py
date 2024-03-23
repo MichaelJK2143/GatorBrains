3f2301981eeafe885bd30ed36bb6357c0bbd6ba7
Tikiri <ekanayake.tikiri@ufl.edu>
2024-03-23 15:25:51 -0400

    adding createAccount

--- Backend/routes.py
+++ Backend/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify  # request
+from flask import jsonify, request, make_response
 from models import User
 
 from app import db
@@ -9,3 +9,14 @@ def configure_routes(app):
     def run():
         users = [{"id": user.id, "name": user.name, "password": user.password, "email": user.email} for user in User.query.all()]
         return jsonify({"users": users})  # type: ignore
+
+    @app.rout('/createAccount', methods=['POST'])
+    def createAccount():
+        try:
+            data = request.get_json()
+            new_user = User(name=data['name'], username=data['username'], password=data['password'], emaiil=data['email'])
+            db.session.add(new_user)
+            db.session.commit()
+            return make_response(jsonify({'message': 'user created'}), 201)
+        except:
+            return make_response(jsonify({'message': 'error creating user'}), 500)
