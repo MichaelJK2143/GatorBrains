ff59fe2fb0f581f3e2d49b10da35e00c67b2f5ba
Joe <joeliu0999@gmail.com>
2024-03-24 06:42:20 -0400

    cant get data, need to display red dots

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,7 +11,7 @@ export default function about() {
             <main>
                 <div style={MainDivStyle}>
                     <TextSection heading="About">
-                        <p>Whether you're seeking fellow students in your area or searching for classmates with similar courses, Gator Brain simplifies the hunt for the perfect study partner.</p>
+                        <p>Whether you are seeking fellow students in your area or searching for classmates with similar courses, Gator Brain simplifies the hunt for the perfect study partner.</p>
                         <p>Gator Brain is the essential study companion for University of Florida students, engineered to streamline the process of finding study partners.</p>
                         <p>With our app, students can effortlessly locate peers in their vicinity or discover individuals sharing their class schedules, fostering a collaborative environment for academic success.</p>
                         <p>Powered by cutting-edge technology and driven by the spirit of community, Gator Brain empowers students to unlock their full potential by connecting with like-minded learners.</p>
--- src/components/map-components/map.jsx
+++ src/components/map-components/map.jsx
@@ -71,7 +71,15 @@ export const MapPlaceholder = () => {
               console.log("Session ID:", session["Session ID"]);
               console.log("Course:", session.Course);
               console.log("Members:", session.Members);
+              console.log("xRatio:", session.x);
+              console.log("yRatio:", session.y);
               // Add more properties as needed
+
+              const xDif=borderCoordinates.right-borderCoordinates.left
+              const yDif=borderCoordinates.bottom-borderCoordinates.top
+              const xcoord=xDif*session.x
+              const ycoord=yDif*session.y
+
             });
           } else {
             console.error("Sessions array not found in response data.");
@@ -100,14 +108,6 @@ export const MapPlaceholder = () => {
         <img src="https://marston.uflib.ufl.edu/files/2023/06/1st-Floor-Map.png"
           style={{ maxWidth: '90%', maxHeight: '90%', margin: 'auto', 'margin-top': '7%' }}
           onMouseMove={handleMouseMove} onClick={handleClick} alt='marston-map'></img>
-        <p>Border Top: {borderCoordinates?.top}</p>
-        <p>Border Right: {borderCoordinates?.right}</p>
-        <p>Border Bottom: {borderCoordinates?.bottom}</p>
-        <p>Border Left: {borderCoordinates?.left}</p>
-        <p>Mouse X: {mousePosition.x}</p>
-        <p>Mouse Y: {mousePosition.y}</p>
-        <p>ratio X: {xRatio}</p>
-        <p>ratio Y: {yRatio}</p>
         <div style={DinoMode}>
           <div style={redDot}></div>
           <input
