83b61cd85e60b9aa664c30a73a78c115c0f3badc
Tikiri <ekanayake.tikiri@ufl.edu>
2024-03-23 22:57:44 -0400

    lotta updates in routes

--- Backend/models.py
+++ Backend/models.py
@@ -10,7 +10,6 @@ class Course(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(80))
      course_code = db.Column(db.String(8))
-     # omg, this could also work if somebody says like MCAT or smthg
      professor = db.Column(db.String(40))
 
      
@@ -21,19 +20,16 @@ class Course(db.Model):
 class StudySesh(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      start_time = db.Column(db.DateTime, default=datetime.now()) # time it was created
-     departure_time = db.Column(db.DateTime, default=lambda: datetime.now() + timedelta(hours=1)) # default to hour later from created
      course = db.Column(db.String(8))
-     x = db.Column(db.Float)
-     y = db.Column(db.Float)
 
-     def __init__(self, duration, course, x, y):
-          self.departure_time = self.start_time + datetime.timedelta(hours=duration)
+     def __init__(self, duration, course, x, y, floor):
           self.course = course
           self.x = x
           self.y = y
+          self.floor = floor
 
-     def __repr___(self):
-          return '<id %r>' % self.course_name
+     def __repr__(self):
+          return '<id %r>' % self.id
      
 class User(db.Model):
      id = db.Column(db.Integer, primary_key=True)
@@ -41,8 +37,8 @@ class User(db.Model):
      username = db.Column(db.String(80))
      password = db.Column(db.String(80))
      email = db.Column(db.String(80)) # figure this out eventually
+     study_sesh_id = db.Column(db.Integer, db.ForeignKey('study_sesh.id'), nullable=True)
      
 
      def __repr__(self):
           return '<User %r>' % self.id
-     
--- Backend/routes.py
+++ Backend/routes.py
@@ -26,68 +26,83 @@ def configure_routes(app):
             return make_response(jsonify({'message': 'New user created! Time to get on the grindddddd'}), 201)
         #except:
             return make_response(jsonify({'message': 'error creating user'}), 500)
-    
-    """
-    @app.route('/<int:user_id>/addCourse', methods=['PUT'])
-    def addCourse(user_id):
-            
-            user = User.query.filter_by(id=user_id).first()
-            if(user):
-                data = request.get_json()
-                if(Course.query.filter_by(course_code = data['course_code']).first()):
-                    user.schedule.append(data['course_id'])
-                    db.session.commit()
-                    return make_response(jsonify({'message': 'course sucessfully added'}), 201)
-                return make_response(jsonify({'message': 'course not found'}), 201)
-            return make_response(jsonify({'message': 'user not found'}), 201)
-            return make_response(jsonify({'message': 'error adding course'}), 500)
-    
-"""
 
-  
+
     # get all courses
     @app.route('/courses', methods=['GET'])
     def getCourses():
-        courses = [{"name": course.name, "course_code": course.course_code, "professor": course.professor} for course in Course.query.all()]
+        courses = [{"Name": course.name, "Course Code": course.course_code, "Professor": course.professor} for course in Course.query.all()]
         return jsonify({"courses": courses})
     
-    @app.route('/createCourse', methods=['POST'])
-    def createCourse():
-        try:
-            data = request.get_json()
-            new_Course = Course(name=data['name'], course_code=data['course_code'], professor=data['professor'])
-            db.session.add(new_Course)
-            db.session.commit()
-            return make_response(jsonify({'message': 'course created'}), 201)
-        except:
-            return make_response(jsonify({'message': 'error creating course'}), 500)
-    
-   
-    
+    # get all current study sessions
+    @app.route('/currentSessions', methods=['GET'])
+    def currentSessions():
+        sessions = [{'Course': session.course, 'Started': session.start_time, 'Members': session.members, 'Floor': session.floor} for session in StudySesh.query.all()]
+        return jsonify({'Sessions': sessions})
 
+    
+    # joining an existing session
+    # make sure data requested is everything abt study session (mainly the id)
+    # do i need to change the <int:user_id> thing? to add the session id too
+
+    @app.route('/<int:user_id>/joinSession', methods=['PUT'])
+    def joinSession(user_id, session_id):
+        sesh = StudySesh.query.filter_by(id=session_id).first()
+        user = User.query.filter_by(id=user_id).first()
+        if(sesh and user):
+            data=request.get_json()
+            sesh.members += 1
+            sesh.users.append(user.id)
+            return make_response(jsonify({'message': "You've joined the session! Time to get on that grind"}, 201))
+        return(make_response(jsonify({'message': 'Session not found :('})))
+
+    """
+    @app.route('/<int:user_id>/addCourse', methods=['PUT'])
+    def addCourse(user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if(user):
+            data = request.get_json()
+            if(Course.query.filter_by(course_code = data['course_code']).first()):
+                user.schedule.append(data['course_id'])
+                db.session.commit()
+                return make_response(jsonify({'message': 'course sucessfully added'}), 201)
+            return make_response(jsonify({'message': 'course not found'}), 201)
+        return make_response(jsonify({'message': 'user not found'}), 201)
+        return make_response(jsonify({'message': 'error adding course'}), 500)
+    """
+
+    # should we add smthg, like plan/schedule a study sesh?
+
+    # leave session route
+        # share pointer? basically delete itself once members == 0 
+    
+    # again, <int:user_id> is correct?
+    # also, should it be GET
+    
+    """
+    @app.route('/<int:user_id>/leaveSession', methods=['GET'])
+    def leaveSession(user_id, session_id):
+        sesh = StudySesh.query.filter_by(id=session_id).first()
+        user = User.query.filter_by(id=user_id).first()
+        if(sesh and user):
+            data=request.get_json()
+            sesh.members -= 1
+            sesh.users.remove(user.id)
+            if(sesh.members == 0):
+
+            return make_response(jsonify({'message': "You've left the session! Was it hard work or hardly working?"}, 201))
+        return(make_response(jsonify({'message': 'Session or user not found :('})))
+    """
 
     # make a new sesh
     @app.route('/createNewStudySession', methods=['POST'])
     def createNewStudySesh():
         try:
             data = request.get_json()
-            new_sesh = StudySesh(course=data['course'], duration=data['hours'])
+            new_sesh = StudySesh(duration=data['hours'], course=data['course'], x=data['x'], y=data['y'], floor=data['floor'])
             db.session.add(new_sesh)
             db.session.commit()
             return make_response(jsonify({'message': 'Study session created! Good luck soldier'}), 201)
         except:
             return make_response(jsonify({'message': 'error creating study session'}), 500)
-        
-
-    #idk if this is post or not
-    """
-    @app.route('/currentSessions', methods=['POST'])
-    def currentSessions():
-        try:
-
-
-
-            return make_response(jsonify({'messsage': 'Here are the current sessions going on'}), 201)
-        except:
-            return make_response(jsonify({'message' : 'error'}), 500)
-    """
+    
